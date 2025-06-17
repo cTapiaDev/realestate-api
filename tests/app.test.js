9dc5fe6d7ac5f550521e77a795bb7e61dbc75aaa
Carlos Tapia <ctapiadev@gmail.com>
2025-06-17 15:22:52 -0400

    test

--- tests/app.test.js
+++ tests/app.test.js
@@ -3,7 +3,7 @@ const app = require('../src/app');
 
 describe('GET /api/inmuebles', () => {
     it('debería retornar una lista de inmuebles', async () => {
-        const rest = await request(app).get('/api/inmuebles');
+        const res = await request(app).get('/api/inmuebles');
         expect(res.statusCode).toBe(200);
         expect(res.body.length).toBeGreaterThan(0);
         expect(res.body[0]).toHaveProperty('direccion');
